e77fe8d5e48c7b6467cffe255a35231091ffc70a
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-01-30 17:19:57 -0300

    hfcb

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import random
+
 
 app = Flask(__name__)
 
@@ -7,13 +8,23 @@ app = Flask(__name__)
 def pg_principal():
     return render_template("principal.html")
 
-@app.route("/sobre")
+@app.route("/sobre", methods=["GET"])
 def pg_sobre():
     return render_template("sobre.html")
 
+@app.route("/login", methods=["GET"])
+def pg_login():
+    return render_template("login.html")
 
+@app.route("/login", methods=["POST"])
+def login_postn():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
 
-
+    if usuario == "Camila" and senha == "1234":
+        return "Você acessou a página restrita"
+    else:
+        return render_template("login.html", erro = "Acesso negado")
 
 
 app.run(debug=True)
